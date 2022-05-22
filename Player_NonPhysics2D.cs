7ee2d05f46d96ce35556a0e70353346b4ddedb98
JeongSeokHyun <codjeongsh02@gmail.com>
2022-05-22 18:52:23 +0900

    Update 함수와 collision 함수 사용

--- Player_NonPhysics2D.cs
+++ Player_NonPhysics2D.cs
@@ -18,12 +18,50 @@ public class Player_NonPhysics2D : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //초기화
+        jumpVy = 0.0f;
+        animIndex = 0;
+        goalCheck = false;
     }
 
+    // 플레이어 캐릭터에 적용된 충돌 판정 영역에 다른 게임 오브젝트의 충돌 판정 영역이 겹쳤다
+    void OncollisionEnter2D(Coollision2D col)
+    {
+        // 들어왔는지 검사
+        if (col.gameObject.name == "Stage_Gate")
+        {
+            //들어왔다
+            goalCheck = true;
+            return;
+        }
+        // 골인 지점이 아닌 곳이라면 스테이지를 다시 로드해서 초기화한다.
+        Application.LoadLevel(Application.loadedLevelName);
+    }
+    // 프레임 다시 쓰기
     // Update is called once per frame
     void Update()
     {
-        
+        // 들어왔는지 검사
+        if (goalCheck)
+        {
+            return; //들어왔다면 처리를 멈춘다.
+        }
+
+        // 현재 플레이어 캐릭터가 어느 높이에 있는지 계산
+        float height = transform.position.y + jumpVy;
+        // 접지 확인(높이가 0이면 접지한 것임)
+        if (height <= 0.0f)
+        {
+            // 점프 초기화
+            height = 0.0f;
+            jumpVy = 0.0f;
+
+            // 점프 확인
+            if (Input.GetButtonDown("Fire1"))
+            {
+                // 점프 처리
+                jumpVy = +1.3f;
+            }
+        }
     }
 }
